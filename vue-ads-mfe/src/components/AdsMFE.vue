759cfda98362c89e1c205d2fb4bcd58c6e14fec4
dshivani10 <117136702+dshivani10@users.noreply.github.com>
2022-11-10 18:36:58 +0530

    Add files via upload

---
+++ vue-ads-mfe/src/components/AdsMFE.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ads-mfe">
+    <p class="ads-title">Ads Banner</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdsMFE',
+}
+</script>
+
+<style scoped>
+.ads-mfe{
+  background-color: #a4c0c4;
+  height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.ads-title{
+  margin: 0;
+}
+</style>
